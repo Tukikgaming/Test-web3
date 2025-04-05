344222f19f196531656048640c9efb32964b28cf
Tukikgaming <160857942+Tukikgaming@users.noreply.github.com>
2025-04-06 00:53:14 +0700

    Update and rename Index.js to index.js

--- Pages/index.js
+++ Pages/index.js
@@ -3,7 +3,7 @@ import { ConnectButton } from '@rainbow-me/rainbowkit';
 export default function Home() {
   return (
     <main style={{ padding: 40 }}>
-      <h1>Web3 Wallet Connect Demo</h1>
+      <h1>Web3 Wallet </h1>
       <ConnectButton />
     </main>
   );
