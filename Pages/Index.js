0709e348af379b365dd868e63c6c58a8ca8bdcec
Tukikgaming <160857942+Tukikgaming@users.noreply.github.com>
2025-04-05 21:54:14 +0700

    Create Index.js

---
+++ Pages/Index.js
@@ -0,0 +1,10 @@
+import { ConnectButton } from '@rainbow-me/rainbowkit';
+
+export default function Home() {
+  return (
+    <main style={{ padding: 40 }}>
+      <h1>Web3 Wallet Connect Demo</h1>
+      <ConnectButton />
+    </main>
+  );
+}
