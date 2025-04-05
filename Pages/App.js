0e04abc3e5f76b2a84e0ca2735bb29d3e87b1f3f
Tukikgaming <160857942+Tukikgaming@users.noreply.github.com>
2025-04-05 21:53:02 +0700

    Create App.js

---
+++ Pages/App.js
@@ -0,0 +1,33 @@
+import '@rainbow-me/rainbowkit/styles.css';
+import { getDefaultWallets, RainbowKitProvider } from '@rainbow-me/rainbowkit';
+import { configureChains, createConfig, WagmiConfig } from 'wagmi';
+import { mainnet, sepolia } from 'wagmi/chains';
+import { publicProvider } from 'wagmi/providers/public';
+import '../styles/globals.css';
+
+const { chains, publicClient } = configureChains(
+  [mainnet, sepolia],
+  [publicProvider()]
+);
+
+const { connectors } = getDefaultWallets({
+  appName: 'My Web3 App',
+  projectId: process.env.NEXT_PUBLIC_WALLETCONNECT_PROJECT_ID,
+  chains,
+});
+
+const wagmiConfig = createConfig({
+  autoConnect: true,
+  connectors,
+  publicClient,
+});
+
+export default function App({ Component, pageProps }) {
+  return (
+    <WagmiConfig config={wagmiConfig}>
+      <RainbowKitProvider chains={chains}>
+        <Component {...pageProps} />
+      </RainbowKitProvider>
+    </WagmiConfig>
+  );
+}
